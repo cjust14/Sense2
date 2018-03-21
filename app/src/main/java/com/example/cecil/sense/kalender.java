0a545aaa1f2a5e2b446136b398e2bd9f1d045d5a
cecil <cecilie.wang@hotmail.com>
2018-03-21 19:15:58 +0100

    nyeste udkast

--- app/src/main/java/com/example/cecil/sense/kalender.java
+++ app/src/main/java/com/example/cecil/sense/kalender.java
@@ -1,13 +1,45 @@
 package com.example.cecil.sense;
 
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
 
 public class kalender extends AppCompatActivity {
-
+private DatePicker date;
+    @TargetApi(Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kalender);
+        date = findViewById(R.id.datePicker);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        date.init(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), new DatePicker.OnDateChangedListener() {
+
+            @Override
+            public void onDateChanged(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                Intent intent3 = new Intent(kalender.this, mma.class);
+                startActivity(intent3);
+            }
+        });
+    }
+
+    public void Logud (View V) {
+        Intent intent1 = new Intent(this, MainActivity.class);
+        startActivity(intent1);
+}
+
+    public void info (View V) {
+        Intent intent2 = new Intent(this, info.class);
+        startActivity(intent2);
     }
 }
+
+
--- app/src/main/java/com/example/cecil/sense/mma.java
+++ app/src/main/java/com/example/cecil/sense/mma.java
@@ -1,7 +1,9 @@
 package com.example.cecil.sense;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class mma extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class mma extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mma);
+    }
+
+    public void tilbage (View V) {
+        Intent intent = new Intent(this, kalender.class);
+        startActivity(intent);
     }
 }
